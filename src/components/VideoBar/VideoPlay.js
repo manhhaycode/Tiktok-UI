21b4de66807410cf3554dd845d4d382474a79897
unknown <manhnvse173470@fpt.edu.vn>
2022-12-16 19:03:02 +0700

    complete video play

--- src/components/VideoBar/VideoPlay.js
+++ src/components/VideoBar/VideoPlay.js
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 import { useEffect, useRef, useState } from 'react';
 import useElementOnScreen from '~/hooks/useElementOnScreen';
 import { actions, useStore } from '~/store';
-import { MutedIcon, PauseIcon, PlayIcon, UnMutedIcon } from '../Icon';
+import { MutedIcon, PauseIcon, PlayIcon, UnMutedIcon, VideoReportIcon } from '../Icon';
 import { Image } from '../Image';
 import styles from './VideoBar.module.scss';
 
@@ -10,19 +10,17 @@ const cx = classNames.bind(styles);
 
 function VideoPlay({ data }) {
     const [state, dispatch] = useStore();
-
     const ref = useRef(null);
     const inputRef = useRef(null);
     const videoRef = useRef();
     let isVisible = useElementOnScreen(ref, { threshold: 0.5 });
-    const [playing, setPlaying] = useState(false);
+    const [playingClick, setPlayingClick] = useState(true);
 
     useEffect(() => {
         if (isVisible) {
-            setPlaying(true);
             videoRef.current.volume = state.volume / 100;
         } else {
-            setPlaying(false);
+            setPlayingClick(true);
         }
     }, [isVisible, state.muted, state.volume]);
 
@@ -30,7 +28,13 @@ function VideoPlay({ data }) {
         <div className={cx('DivVideoWrapper')}>
             <div className={cx('wrapper-video-play')}>
                 <canvas width="56" height="100" className={cx('canvas')}></canvas>
-                <div className={cx('video-card--container')} ref={ref}>
+                <div
+                    className={cx('video-card--container')}
+                    ref={ref}
+                    onMouseOver={() => {
+                        setTimeout(() => {});
+                    }}
+                >
                     <Image src={data.thumb_url} className={cx('thumb-video')}></Image>
                     {isVisible && (
                         <div className={cx('video-play--container')}>
@@ -46,19 +50,23 @@ function VideoPlay({ data }) {
                                 loop
                                 webkitplaysinline="true"
                             ></video>
+                            <div className={cx('video-report')}>
+                                <VideoReportIcon />
+                                Report
+                            </div>
                             <div
                                 className={cx('video-controls-statePlaying')}
                                 onClick={() => {
-                                    if (playing) {
+                                    if (playingClick) {
                                         videoRef.current.pause();
-                                        setPlaying(false);
+                                        setPlayingClick(false);
                                     } else {
-                                        setPlaying(true);
+                                        setPlayingClick(true);
                                         videoRef.current.play();
                                     }
                                 }}
                             >
-                                {playing ? <PlayIcon /> : <PauseIcon />}
+                                {playingClick ? <PlayIcon /> : <PauseIcon />}
                             </div>
                             <div className={cx('video-controls-volume')}>
                                 <div className={cx('sound-controls')}>
@@ -80,7 +88,6 @@ function VideoPlay({ data }) {
                                         max="100"
                                         step="1"
                                         onInput={(e) => {
-                                            console.log(e.target.value);
                                             dispatch(actions.setVolume(e.target.value));
                                         }}
                                     />
