7a5076abc7cacf0b1feaac08e040f15785357ecc
unknown <manhnvse173470@fpt.edu.vn>
2022-12-20 09:50:59 +0700

    upload

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,7 +2,17 @@ import { forwardRef } from 'react';
 import images from '~/assets/image';
 
 function Image({ className, src, fallback = images.noImage, alt }, ref) {
-    return <img className={className} ref={ref} src={src || fallback} alt={alt}></img>;
+    return (
+        <img
+            className={className}
+            ref={ref}
+            src={src || fallback}
+            alt={alt}
+            onError={(e) => {
+                e.target.src = fallback;
+            }}
+        ></img>
+    );
 }
 
 export default forwardRef(Image);
