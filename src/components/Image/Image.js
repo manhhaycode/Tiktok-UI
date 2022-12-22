861a524dec842fe234cccca5aea2f298eec4aac3
unknown <manhnvse173470@fpt.edu.vn>
2022-12-22 11:25:46 +0700

    update Image component

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -4,7 +4,7 @@ import images from '~/assets/image';
 function Image({ className, src, fallback = images.tiktok, alt }, ref) {
     return (
         <img
-            style={{ backgroundColor: 'black' }}
+            style={{ backgroundColor: 'black', objectFit: 'cover' }}
             className={className}
             ref={ref}
             src={src || fallback}
